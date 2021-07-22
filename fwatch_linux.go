9171f2f1c0695a1338be720404953916e100a2ac
wangoo <wongoo@apache.org>
2021-07-22 15:05:53 +0800

    fix lint

--- fwatch_linux.go
+++ fwatch_linux.go
@@ -3,12 +3,12 @@ package fwatch
 import "golang.org/x/sys/unix"
 
 const (
-	// FileCreateDeleteEvents events for file create and delete
+	// FileCreateDeleteEvents events for file create and delete.
 	FileCreateDeleteEvents = unix.IN_CREATE |
 		unix.IN_MOVE | unix.IN_MOVED_TO | unix.IN_MOVED_FROM | unix.IN_MOVE_SELF |
 		unix.IN_DELETE | unix.IN_DELETE_SELF
 
-	// FileWriteDeleteEvents events for file write and delete
+	// FileWriteDeleteEvents events for file write and delete.
 	FileWriteDeleteEvents = unix.IN_MODIFY |
 		unix.IN_MOVE | unix.IN_MOVED_TO | unix.IN_MOVED_FROM | unix.IN_MOVE_SELF |
 		unix.IN_DELETE | unix.IN_DELETE_SELF
